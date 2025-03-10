e74754a6f9a5b0cad58286ad98535995d3af6764
meetkamal256 <meetkamal256@gmail.com>
2025-03-10 15:37:17 +0100

    fix build error

--- src/okta.config.tsx
+++ src/okta.config.tsx
@@ -1,9 +1,9 @@
 import { OktaAuth } from "@okta/okta-auth-js";
 
 const oktaAuth = new OktaAuth({
-  clientId: "0oanr0z68mdPcEwQf5d7",
-  issuer: "https://dev-04974889.okta.com/oauth2/ausnr11h6wUtWoKPf5d7",
-  redirectUri: "http://localhost:5173/login/callback",
+  clientId: import.meta.env.VITE_OKTA_CLIENT_ID,
+  issuer: import.meta.env.VITE_OKTA_ISSUER,
+  redirectUri: import.meta.env.VITE_OKTA_REDIRECT_URI,
   scopes: ["openid", "profile", "email"],
   pkce: true,
   restoreOriginalUri: async (_oktaAuth, originalUri) => {
